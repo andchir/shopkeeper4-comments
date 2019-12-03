48758cdd74d1755d0d7be0a4ec1b1d3987b250d9
Andrei <andchir@gmail.com>
2019-12-03 15:33:25 +0300

    abstract classes update

--- Repository/CommentAdminRepositoryAbstract.php
+++ Repository/CommentAdminRepositoryAbstract.php
@@ -6,6 +6,21 @@ use App\Repository\BaseRepository;
 
 abstract class CommentAdminRepositoryAbstract extends BaseRepository
 {
+    /**
+     * @param Builder $query
+     * @param $searchWord
+     */
+    public function addSearchQuery(&$query, $searchWord)
+    {
+        if (!$searchWord) {
+            return;
+        }
+        $query->addOr(
+            $query->expr()
+                ->addOr($query->expr()->field('comment')->equals(new \MongoDB\BSON\Regex("{$searchWord}", "i")))
+                ->addOr($query->expr()->field('reply')->equals(new \MongoDB\BSON\Regex("{$searchWord}", "i")))
+        );
+    }
 
     /**
      * @param string $threadId
