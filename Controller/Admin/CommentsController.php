72b9840e92a3293bfe86c2c6821d0cf838baef88
Andrei <andchir@gmail.com>
2019-12-06 14:01:59 +0300

    Admin\CommentsController class updated

---
+++ Controller/Admin/CommentsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Andchir\CommentsBundle\Controller\Admin;
+
+use Andchir\CommentsBundle\Document\CommentInterface;
+use Andchir\CommentsBundle\Repository\CommentRepositoryAbstract;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+if (class_exists('\App\Controller\Admin\StorageControllerAbstract')) {
+
+    /**
+     * Class CommentsController
+     * @Route("/admin/comments")
+     */
+    class CommentsController extends \App\Controller\Admin\StorageControllerAbstract {
+
+        /**
+         * @param $data
+         * @param int $itemId
+         * @return array
+         */
+        public function validateData($data, $itemId = null)
+        {
+            if (empty($data)) {
+                return ['success' => false, 'msg' => 'Data is empty.'];
+            }
+            return ['success' => true];
+        }
+
+        /**
+         * @param $data
+         * @param int $itemId
+         * @return JsonResponse
+         */
+        public function createUpdate($data, $itemId = null)
+        {
+            /** @var TranslatorInterface $translator */
+            $translator = $this->get('translator');
+            /** @var \Doctrine\ODM\MongoDB\DocumentManager $dm */
+            $dm = $this->get('doctrine_mongodb')->getManager();
+
+            /** @var CommentInterface $item */
+            if($itemId){
+                /** @var CommentInterface $item */
+                $item = $this->getRepository()->find($itemId);
+                if(!$item){
+                    return $this->setError($translator->trans('Item not found.', [], 'validators'));
+                }
+            } else {
+                $item = new Comment();
+                $item->setAuthor($this->getUser());
+            }
+
+            $item
+                ->setStatus($data['status'])
+                ->setThreadId($data['threadId'])
+                ->setVote($data['vote'])
+                ->setComment($data['comment'])
+                ->setReply($data['reply']);
+
+            if (!$item->getId()) {
+                $dm->persist($item);
+            }
+            $dm->flush();
+
+            return $this->json($item, 200, [], ['groups' => ['details']]);
+        }
+
+        /**
+         * @return CommentRepositoryAbstract
+         */
+        public function getRepository()
+        {
+            return $this->get('doctrine_mongodb')
+                ->getManager()
+                ->getRepository('App\MainBundle\Document\Comment');
+        }
+    }
+
+} else {
+    class CommentsController extends AbstractController { }
+}
